82a9d6b52e9cd85ab92dbc2fdafbd7c041098981
eslam <eslam556547@gmail.com>
2024-09-19 08:47:40 +0000

    add storage and comoser.lock

---
+++ storage/framework/views/027869f45af8954d3efff6f1dbc67001.php
@@ -0,0 +1,1 @@
+<?php /**PATH /home/u101566037/domains/ibdl.net/public_html/blog/vendor/filament/filament/src/../resources/views/components/layouts/app/sidebar/end.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/4c4da1c4b20ac1ad673c093b573da4ce.php
@@ -0,0 +1,8 @@
+<h1
+    <?php echo e($attributes->class(['filament-header-heading text-2xl font-bold tracking-tight'])); ?>
+
+>
+    <?php echo e($slot); ?>
+
+</h1>
+<?php /**PATH /home/u101566037/domains/ibdl.net/public_html/blog/vendor/filament/filament/src/../resources/views/components/header/heading.blade.php ENDPATH**/ ?>
